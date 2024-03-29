a82437411e69d5d83a333c3557c05cb337c538c2
Mahesh <161496894+maheshpatil53@users.noreply.github.com>
2024-03-29 01:41:46 +0000

    Created House price predictor

---
+++ app.py
@@ -0,0 +1,20 @@
+import numpy as np
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+cv = pickle.load(open("models/house_model.pkl",'rb'))
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method=='POST':
+        v1=int(request.form['area'])
+        v2=int(request.form['bedroom'])
+        v3=np.array([[v1,v2]])
+        prediction = cv.predict(v3)
+        return render_template('index.html', prediction=prediction)
+    else:
+        return render_template('index.html')
+  
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
